045b6abd02b8eea8f6e11e37427f4676db351b74
skrint <karelkin2004@gmail.com>
2024-06-05 18:30:46 +0300

    added: LS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 (() => {
-  const arr = [];
+  arr = getArr();
+  arr = arr ? jsonToData(arr) : [];
 
   const textTemplate = document.querySelector('#template__list').content;
   const movieItemTemplate = textTemplate.querySelector('.movie__item');
@@ -7,10 +8,15 @@
   const $inputText = document.querySelector('.js-input');
   const $form = document.querySelector('.app');
   const $ul = document.querySelector('.movie__list');
+
   let items = $ul.children;
 
   const $img = document.createElement('img');
   $img.src = './img/btn-delete.svg';
+
+  function clearInput() {
+    $inputText.value = '';
+  }
 
   function getTextFromUser() {
     if (!$inputText.value.trim()) {
@@ -21,16 +27,27 @@
     return text;
   }
 
-  function addTextInArray(text, isDone, img) {
-    arr.push({
-      text: text,
-      isDone: isDone,
-      img: img,
-    });
+  function saveArr(arr, keyName) {
+    localStorage.setItem(keyName, JSON.stringify(arr));
   }
 
-  function clearInput() {
-    $inputText.value = '';
+  function jsonToData(data) {
+    return JSON.parse(data);
+  }
+
+  function getArr() {
+    return localStorage.getItem('movies');
+  }
+
+  function addTextInArray(text, isDone, img) {
+    saveArr(
+      arr.push({
+        text: text,
+        isDone: isDone,
+        img: img,
+      }),
+      'movies'
+    );
   }
 
   function functionalEvents(item, selector, eventElement) {
@@ -49,11 +66,35 @@
         text.style.textDecoration = 'line-through';
         text.style.color = 'grey';
       }
+      saveArr(arr, 'movies');
     });
   }
 
-  $form.addEventListener('submit', (e) => {
-    e.preventDefault();
+  function init(arr) {
+    if (arr.length > 0) {
+      arr.forEach((movie) => {
+        const newItem = movieItemTemplate.cloneNode(true);
+        const itemDescr = newItem.querySelector('.movie__item-text');
+        itemDescr.textContent = movie.text;
+        if (movie.isDone) {
+          const svgPath = newItem.querySelector('.svg__path');
+          const radioButton = newItem.querySelector('.js-input-radio');
+          newItem.style.backgroundColor = '#2d2d2d';
+          svgPath.style.fill = 'grey';
+          itemDescr.style.textDecoration = 'line-through';
+          itemDescr.style.color = 'grey';
+          radioButton.checked = true;
+        }
+        $ul.appendChild(newItem);
+        functionalEvents(newItem, '.js-btn-remove', 'click');
+        functionalEvents(newItem, '.js-input-radio', 'change');
+      });
+    }
+  }
+
+  init(arr);
+
+  function createItem() {
     const textFromUser = getTextFromUser();
     if (!textFromUser) {
       return;
@@ -65,5 +106,11 @@
     functionalEvents(newItem, '.js-btn-remove', 'click');
     functionalEvents(newItem, '.js-input-radio', 'change');
     addTextInArray(textFromUser, false, $img.src);
+    saveArr(arr, 'movies');
+  }
+
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createItem();
   });
 })();
